84b2fb05bb1051a7b4cd75716294e8625cc26481
Mateus S Cavalheiro <mateusdossantoscavalheiro@gmail.com>
2026-03-29 22:43:42 -0300

    fix: deploy by Render

---
+++ src/main/java/DatabaseConfig.java
@@ -0,0 +1,18 @@
+public class DatabaseConfig {
+
+    private static final String SUPABASE_URI = "jdbc:postgresql://postgres:%zVN/.2BzF$rUKK@db.mvjpcsmurtsimsysrexf.supabase.co:5432/postgress";
+    private static final String SUPABASE_USER = "postgres";
+    private static final String SUPABASE_PASS = "%zVN/.2BzF$rUKK";
+
+    public static final String URL = System.getenv("DATABASE_URL") != null
+            ? System.getenv("DATABASE_URL")
+            : SUPABASE_URI;
+
+    public static final String USER = System.getenv("DATABASE_USER") != null
+            ? System.getenv("DATABASE_USER")
+            : SUPABASE_USER;
+
+    public static final String PASS = System.getenv("DATABASE_PASS") != null
+            ? System.getenv("DATABASE_PASS")
+            : SUPABASE_PASS;
+}
